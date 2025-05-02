4ba703fa9d540a8a12b130ccaec3a712b39c8653
Vineeth Varghese <vineethin@gmail.com>
2025-05-01 23:13:36 -0400

    updated Auto Test generator

---
+++ vinsuite-backend/src/main/java/com/vinsuite/dto/AutomationRequest.java
@@ -0,0 +1,40 @@
+package com.vinsuite.dto;
+
+public class AutomationRequest {
+    private String testCase;
+    private String htmlCode;
+    private String language;
+    private String framework;
+
+    public String getTestCase() {
+        return testCase;
+    }
+
+    public void setTestCase(String testCase) {
+        this.testCase = testCase;
+    }
+
+    public String getHtmlCode() {
+        return htmlCode;
+    }
+
+    public void setHtmlCode(String htmlCode) {
+        this.htmlCode = htmlCode;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getFramework() {
+        return framework;
+    }
+
+    public void setFramework(String framework) {
+        this.framework = framework;
+    }
+}
