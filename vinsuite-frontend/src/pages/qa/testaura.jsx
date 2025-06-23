4ddd0194ba724583ccbbc8822e16002d2dc2db4f
vineeth varghese <vineeth@corestudycast.com>
2025-06-23 15:47:40 -0400

    Adding TestAura

---
+++ vinsuite-frontend/src/pages/qa/testaura.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TestAuraPanel from "../../components/qa/TestAuraPanel";
+
+const TestAuraPage = () => {
+  return <TestAuraPanel />;
+};
+
+export default TestAuraPage;
