16d332272c64e0c138e2e03ab627696638aa5b74
vineeth varghese <vineeth@corestudycast.com>
2025-06-30 13:04:36 -0400

    Updated Config

--- vinsuite-backend/src/main/java/com/vinsuite/config/WebConfig.java
+++ vinsuite-backend/src/main/java/com/vinsuite/config/WebConfig.java
@@ -1,9 +1,8 @@
 package com.vinsuite.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,15 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     public WebConfig(TestAuraConfig config) {
         this.config = config;
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://vinsuite360.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
     }
 
     @Override
