9bf68ed576a0d6f58e6bea4fdbdcc4e4485101ee
Vineethin <vineethin@gmail.com>
2025-06-30 20:05:53 -0400

    Removed legacy vinsuite-backend after renaming to inquisiAI-backend

--- vinsuite-backend/src/main/java/com/vinsuite/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vinsuite.repository;
-
-import com.vinsuite.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-    List<Project> findByCreatedById(Long userId);
-}
